fb5db38c1b0e79cdb8f8a779cd5bff8a2f10a062
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-11-12 11:27:17 +1300

    display the new component ok - API not working yet

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const server = express()
 server.use(express.static(path.join(__dirname, '..', 'public')))
 server.use(express.json())
 
+// server.use('/timeline', require('./routes/timeline'))
+
+
 server.get('/test', (req, res) => {
 res.send('test test test test')
 })
