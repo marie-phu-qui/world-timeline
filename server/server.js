a072dad5c35b534bb8c92e59f346629ba24f26a3
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-11-13 05:32:35 +1300

    api runs on server side/postman

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const path = require('path')
 const server = express()
-
+const db = require('./db/db')
 
 server.use(express.static(path.join(__dirname, '..', 'public')))
 server.use(express.json())
 
-// server.use('/timeline', require('./routes/timeline'))
+// server.get('/timeline', require('./routes/timeline'))
 
+server.get('/timeline', (req, res) => {
+    db.getEvents()
+      .then(events => {
+        console.log(events)
+        res.json(events)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
 
 server.get('/test', (req, res) => {
 res.send('test test test test')
