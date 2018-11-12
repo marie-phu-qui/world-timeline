c3093be9bb85eaef9d243c98e110f64df0ced9d1
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-11-13 06:38:50 +1300

    added content to each time card

--- client/components/Timeline.jsx
+++ client/components/Timeline.jsx
@@ -7,40 +7,128 @@ const Timeline = () => {
         <h4>Choose time laps</h4>
         <div  className="scroll-container">
             <section className="card">
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
-                <div className="card--content">
-                </div>
+                <div className="card--content BCE">
+                40th BCE</div>
+                <div className="card--content BCE">
+                39th BCE</div>
+                <div className="card--content BCE">
+                38th BCE</div>
+                <div className="card--content BCE">
+                37th BCE</div>
+                <div className="card--content BCE">
+                36th BCE</div>
+                <div className="card--content BCE">
+                35th BCE</div>
+                <div className="card--content BCE">
+                34th BCE</div>
+                <div className="card--content BCE">
+                33rd BCE</div>
+                <div className="card--content BCE">
+                32nd BCE</div>
+                <div className="card--content BCE">
+                31st BCE</div>
+                <div className="card--content BCE">
+                30th BCE</div>
+                <div className="card--content BCE">
+                29th BCE</div>
+                <div className="card--content BCE">
+                28th BCE</div>
+                <div className="card--content BCE">
+                27th BCE</div>
+                <div className="card--content BCE">
+                26th BCE</div>
+                <div className="card--content BCE">
+                25th BCE</div>
+                <div className="card--content BCE">
+                24th BCE</div>
+                <div className="card--content BCE">
+                23rd BCE</div>
+                <div className="card--content BCE">
+                22nd BCE</div>
+                <div className="card--content BCE">
+                21st BCE</div>
+                <div className="card--content BCE">
+                20th BCE</div>
+                <div className="card--content BCE">
+                19th BCE</div>
+                <div className="card--content BCE">
+                18th BCE</div>
+                <div className="card--content BCE">
+                17th BCE</div>
+                <div className="card--content BCE">
+                16th BCE</div>
+                <div className="card--content BCE">
+                15th BCE</div>
+                <div className="card--content BCE">
+                14th BCE</div>
+                <div className="card--content BCE">
+                13rd BCE</div>
+                <div className="card--content BCE">
+                12nd BCE</div>
+                <div className="card--content BCE">
+                11st BCE</div>
+                <div className="card--content BCE">
+                10th BCE</div>
+                <div className="card--content BCE">
+                9th BCE</div>
+                <div className="card--content BCE">
+                8th BCE</div>
+                <div className="card--content BCE">
+                7th BCE</div>
+                <div className="card--content BCE">
+                6th BCE</div>
+                <div className="card--content BCE">
+                5th BCE</div>
+                <div className="card--content BCE">
+                4th BCE</div>
+                <div className="card--content BCE">
+                3rd BCE</div>
+                <div className="card--content BCE">
+                2nd BCE</div>
+                <div className="card--content BCE">
+                1st BCE</div>
+                <div className="card--content CE">
+                1st  CE</div>
+                <div className="card--content CE">
+                2nd CE</div>
+                <div className="card--content CE">
+                3rd CE</div>
+                <div className="card--content CE">
+                4th CE</div>
+                <div className="card--content CE">
+                5th CE</div>
+                <div className="card--content CE">
+                6th CE</div>
+                <div className="card--content CE">
+                7th CE</div>
+                <div className="card--content CE">
+                8th CE</div>
+                <div className="card--content CE">
+                9th CE</div>
+                <div className="card--content CE">
+                10th CE</div>
+                <div className="card--content CE">
+                11th CE</div>
+                <div className="card--content CE">
+                12th CE</div>
+                <div className="card--content CE">
+                13th CE</div>
+                <div className="card--content CE">
+                14th CE</div>
+                <div className="card--content CE">
+                15th CE</div>
+                <div className="card--content CE">
+                16th CE</div>
+                <div className="card--content CE">
+                17th CE</div>
+                <div className="card--content CE">
+                18th CE</div>
+                <div className="card--content CE">
+                19th CE</div>
+                <div className="card--content CE">
+                20th CE</div>
+                <div className="card--content CE">
+                21st CE</div>
             </section>
         </div>
         </div>
